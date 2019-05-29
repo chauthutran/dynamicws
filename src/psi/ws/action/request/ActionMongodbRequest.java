2d54fdb49c0fc314565af857f0ad35c9a798962d
psi-mis <cthutran@gmail.com>
2019-05-29 12:54:51 +0900

    Mongodb actions

--- src/psi/ws/action/request/ActionMongodbRequest.java
+++ src/psi/ws/action/request/ActionMongodbRequest.java
@@ -1,5 +1,8 @@
 package psi.ws.action.request;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import psi.ws.action.Action;
 import psi.ws.action.ActionRequest;
 import psi.ws.configuration.Configuration;
@@ -54,17 +57,26 @@ public class ActionMongodbRequest extends ActionRequest
         if( this.requestType.equals( Util.REQUEST_TYPE_POST ))
         {
             this.connection.insert( action.getInput().getInputJson() );
+            action.getOutput().setOutputMsg( "SUCCESS" );
         }
         else if( this.requestType.equals( Util.REQUEST_TYPE_PUT ))
         {
             this.connection.update( action.getInput().getInputJson() );
+            action.getOutput().setOutputMsg( "SUCCESS" );
         }
         else if( this.requestType.equals( Util.REQUEST_TYPE_DELETE ))
         {
             this.connection.delete( action.getInput().getInputJson() );
+            action.getOutput().setOutputMsg( "SUCCESS" );
         }
-        
-        action.getOutput().setOutputMsg( action.getInput().getInputStr() );
+        else if( this.requestType.equals( Util.REQUEST_TYPE_GET ))
+        {
+            JSONObject inputJson = action.getInput().getInputJson();
+            JSONArray list = this.connection.get( inputJson.getJSONArray( "conditions" )  );
+            JSONObject result = new JSONObject();
+            result.put( "result", list );
+            action.getOutput().setOutputMsg( result.toString() );
+        }
         
         this.connection.close();
     }
--- src/psi/ws/mongodb/MongoConnection.java
+++ src/psi/ws/mongodb/MongoConnection.java
@@ -2,30 +2,40 @@ package psi.ws.mongodb;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import psi.ws.configuration.Configuration;
 import psi.ws.configuration.MongodbConfiguration;
 import psi.ws.exception.ActionException;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.TextSearchOptions;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import com.mongodb.util.JSON;
 
 public class MongoConnection
 {
     private MongodbConfiguration configuration;
     private MongoClient mongoClient;
     private MongoCollection<Document> collection;
+    private DBCollection dbCollection;
 
     public MongoConnection( Configuration configuration )
     {
@@ -44,8 +54,13 @@ public class MongoConnection
             MongoClientURI uri = this.getURI();
             this.mongoClient = new MongoClient( uri );
             
-            MongoDatabase db = mongoClient.getDatabase( this.configuration.getDbName() );
-            this.collection = db.getCollection( this.configuration.getCollectionName() );
+            MongoDatabase database = mongoClient.getDatabase( this.configuration.getDbName() );
+            this.collection = database.getCollection( this.configuration.getCollectionName() );
+            
+            
+            DB db = mongoClient.getDB( this.configuration.getDbName() );
+            this.dbCollection = db.getCollection( this.configuration.getCollectionName() );
+            
         }
         catch ( Exception ex )
         {
@@ -71,9 +86,9 @@ public class MongoConnection
         return ( result.getModifiedCount() > 0 );
     }
 
-    public boolean delete( String property, String value )
+    public boolean delete( String property, String value, String operator )
     {
-        DeleteResult result = this.collection.deleteMany( this.filter( property, value ) );
+        DeleteResult result = this.collection.deleteMany( makeFilter( property, value, operator ) );
         return ( result.getDeletedCount() == 1 );
     }
     
@@ -84,17 +99,95 @@ public class MongoConnection
         return ( result.getDeletedCount() == 1 );
     }
     
-    public List<JSONObject> get( String property, String value )
+    public JSONArray get( JSONArray conditions )
     {
-//        BasicQuery query1 = new BasicQuery("{ age : { $lt : 40 }, name : 'cat' }");
+        JSONArray list = new JSONArray();
         
-        List<JSONObject> searchResult = new ArrayList<JSONObject>();
+        BasicDBObject andQuery = new BasicDBObject();
+        List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+        for( int i = 0; i < conditions.length(); i++ )
+        {
+            JSONObject condition = conditions.getJSONObject( i );
+            String key = condition.getString( "key" );
+            String value = condition.getString( "value" );
+            String operator = condition.getString( "operator" );
+          
+            obj.add( new BasicDBObject( key, new BasicDBObject("$" + operator, value ) ) );
+        }
         
-        this.collection.find( this.filter( property, value )).forEach((Consumer<? super Document>) (Document doc) -> {
-            searchResult.add( new JSONObject( doc.toJson() ) );
-        });
         
-       return searchResult;
+        andQuery.put("$and", obj);
+        
+//        System.out.println(andQuery.toString());
+        
+        DBCursor cursor = this.dbCollection.find( andQuery );           
+        while(cursor.hasNext()){
+           String result = cursor.next().toString();
+//           JSONObject output = new JSONObject(JSON.serialize(result));
+           list.put( new JSONObject( result ) );
+        }
+        
+        return list;
+        
+//        List<Bson> filters = new ArrayList<Bson>();
+//        for( int i = 0; i < conditions.length(); i++ )
+//        {
+//            JSONObject condition = conditions.getJSONObject( i );
+//            String key = condition.getString( "key" );
+//            String value = condition.getString( "value" );
+//            String operator = condition.getString( "operator" );
+//            
+//            Bson filter = makeFilter( key, value, operator );
+//            filters.add( filter );
+//        }
+//        
+//        
+//        
+//        AggregateIterable<Document> aggregate = this.collection.aggregate( filters );
+//
+//        List<JSONObject> list = new ArrayList<JSONObject>();
+//        MongoCursor<Document> iterator = aggregate.iterator();
+//        while (iterator.hasNext()) {
+//            String data = iterator.next().toJson();
+//            list.add( new JSONObject( data ) );
+//        }
+//        
+//        return list;
+        
+        
+//        JSONArray list = new JSONArray();
+//        
+//        if( conditions.length() > 1 )
+//        {
+//            List<BasicDBObject> searchArguments = new ArrayList<BasicDBObject>();
+//            
+//            for( int i = 0; i < conditions.length(); i++ )
+//            {
+//                JSONObject condition = conditions.getJSONObject( i );
+//                
+//                
+//                String key = condition.getString( "key" );
+//                String value = condition.getString( "value" );
+//                String operator = condition.getString( "operator" );
+//
+//                searchArguments.add(new BasicDBObject( key ,new BasicDBObject( "$" + operator, value )));
+//             }
+//
+//            BasicDBObject searchObject = new BasicDBObject();
+//            searchObject.put("$and", searchArguments);
+//            DBCursor logicalQueryResults = this.dbCollection.find( searchObject );           
+//            while(logicalQueryResults.hasNext()){
+//                 String result = logicalQueryResults.next().toString();
+////                 JSONObject output = new JSONObject(JSON.serialize(result));
+//                 list.put( new JSONObject( result ) );
+//            }
+//        }
+//        else if( conditions.length() > 1 )
+//        {
+//            
+//        }
+//        
+//        return list;
     }
     
     // -------------------------------------------------------------------------
@@ -108,8 +201,38 @@ public class MongoConnection
             + this.configuration.getDbName() + "?retryWrites=true" );
     }
     
-    private Bson filter( String property, String value )
+    private Bson makeFilter( String property, String value, String operator )
     {
-        return Filters.eq( property, value );
+        if( operator.equals( "eq" ))
+        {
+            return Filters.eq( property, value );
+        }
+        else if( operator.equals( "ilike" ))
+        {
+            return Filters.text( value, new TextSearchOptions().caseSensitive( false ) );
+        }
+        else if( operator.equals( "like" ))
+        {
+            return Filters.text( value, new TextSearchOptions().caseSensitive( true ) );
+        }
+        else if( operator.equals( "lt" ))
+        {
+            return Filters.lt( property, value );
+        }
+        else if( operator.equals( "lte" ))
+        {
+            return Filters.lte( property, value );
+        }
+        else if( operator.equals( "gt" ))
+        {
+            return Filters.gt( property, value );
+        }
+        else if( operator.equals( "gte" ))
+        {
+            return Filters.gte( property, value );
+        }
+        
+        return null;
     }
+    
 }
